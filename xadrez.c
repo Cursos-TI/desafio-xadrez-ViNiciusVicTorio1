e56e9a8ea48d6ef5c47c0c2bcfa62e94ddff41df
ViNiciusVicTorio1 <161782159+ViNiciusVicTorio1@users.noreply.github.com>
2024-12-18 01:37:28 +0000

    commit nivel mestre

--- xadrez.c
+++ xadrez.c
@@ -5,36 +5,59 @@
 #define TORRE_MOV 5
 #define RAINHA_MOV 8
 
+// Função recursiva para movimentação do Bispo
+void moverBispo(int movimentos) {
+    if (movimentos > 0) {
+        printf("Diagonal Superior Direita\n");
+        moverBispo(movimentos - 1);
+    }
+}
+
+// Função recursiva para movimentação da Torre
+void moverTorre(int movimentos) {
+    if (movimentos > 0) {
+        printf("Direita\n");
+        moverTorre(movimentos - 1);
+    }
+}
+
+// Função recursiva para movimentação da Rainha
+void moverRainha(int movimentos) {
+    if (movimentos > 0) {
+        printf("Esquerda\n");
+        moverRainha(movimentos - 1);
+    }
+}
+
 int main() {
     // Movimentação do Bispo
     printf("Movimentação do Bispo:\n");
-    for (int i = 0; i < BISPO_MOV; i++) {
-        printf("Diagonal Superior Direita\n");
-    }
+    moverBispo(BISPO_MOV);
 
     // Movimentação da Torre
     printf("\nMovimentação da Torre:\n");
-    for (int i = 0; i < TORRE_MOV; i++) {
-        printf("Direita\n");
-    }
+    moverTorre(TORRE_MOV);
 
     // Movimentação da Rainha
     printf("\nMovimentação da Rainha:\n");
-    for (int i = 0; i < RAINHA_MOV; i++) {
-        printf("Esquerda\n");
-    }
+    moverRainha(RAINHA_MOV);
 
     // Movimentação do Cavalo
     printf("\nMovimentação do Cavalo:\n");
 
-    // Loop externo usando for para a movimentação para baixo
+    // Loop aninhado com variáveis múltiplas e condições múltiplas
     for (int i = 0; i < 2; i++) {
-        printf("Baixo\n");
-    }
-
-    // Loop interno usando for para a movimentação para a esquerda
-    for (int j = 0; j < 1; j++) {
-        printf("Esquerda\n");
+        for (int j = 0; j < 2; j++) {
+            if (i == 1 && j == 1) {
+                printf("Direita\n");
+                break;
+            }
+            if (i == 0) {
+                printf("Cima\n");
+                continue;
+            }
+            printf("Direita\n");
+        }
     }
 
     return 0;
